0d8690c64e6f5e7463d45eb22d25d2417eca3e9e
Saskar Khadka <saskar.khadka@gmail.com>
2023-04-21 16:16:27 +0545

    lab report 2

--- Lab 2/src/main.py
+++ Lab 2/src/main.py
@@ -1,4 +1,15 @@
 from graph import plot_graph
+from insertion_sort import insertion_sort
+from merge_sort import merge_sort
 
 if __name__ == "__main__":
+    array = [4, 6, 1, 7, 2, 9, 3]
+    print("Array before insertion sort:", array)
+    insertion_sort(array)
+    print("Array after insertion sort:", array)
+
+    array = [4, 6, 1, 7, 2, 9, 3]
+    print("Array before merge sort:", array)
+    merge_sort(array, 0, len(array) - 1)
+    print("Array after merge sort:", array)
     plot_graph()
